6ab9a2baed037adf1161aaa7fdeb12f92bb913ab
Andrej Lavrinovic <andrei.lavrinovic@gmail.com>
2017-07-18 22:53:29 +0100

    Create files

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello Andrej!!!
+            <p>How are you today?</p>
+         </div>
+      );
+   }
+}
+
+export default App;
